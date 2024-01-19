f577a6b1f6557ea5affd9c2d97df01f1b4971150
Irving Meng <imeng@lcs.on.ca>
2024-01-19 08:40:19 -0500

    Initial Commit

---
+++ trading card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trading card
+//
+//  Created by 孟元森 on 2024-01-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trading card/trading_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  trading_cardApp.swift
+//  trading card
+//
+//  Created by 孟元森 on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct trading_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
